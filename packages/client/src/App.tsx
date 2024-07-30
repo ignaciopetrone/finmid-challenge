a03b2142ec4e3f1aa5514e89be7dbb00aef4736f
Nacho Petrone <juanignaciopetrone@gmail.com>
2024-07-30 18:22:28 +0200

    FINMID-002: Creat CallApi util function, create a draft of  login resolver and execute it to test retriving the token

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,33 @@
+import { useEffect } from "react";
+import callApi from "./utils/callApi";
+
+type LoginPayload = {
+  email: string;
+  password: string;
+};
+
+const login = async ({ email, password }: LoginPayload) => {
+  try {
+    const { token } = await callApi({
+      method: "POST",
+      endpoint: "/login",
+      payload: { email, password },
+    });
+    return token;
+  } catch (error) {}
+};
+
 const App = () => {
+  useEffect(() => {
+    (async () => {
+      const token = await login({
+        email: "gandalf.the.grey@test.com",
+        password: "123code",
+      });
+      console.log("token", token);
+    })();
+  }, []);
+
   return (
     <div className="app">
       <div className="app__header">HEADER</div>
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.scss";
 
+// Removed strict mode to avoid doble rendering in development mode
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <App />
 );
