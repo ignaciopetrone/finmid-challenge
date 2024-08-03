482c32dd502fa394097a66f0a99456a01c90bd26
Nacho Petrone <juanignaciopetrone@gmail.com>
2024-08-03 18:46:39 +0200

    FINMID-017: Update formatDate function

--- packages/client/src/utils/formats.ts
+++ packages/client/src/utils/formats.ts
@@ -3,18 +3,22 @@ export const formatPrice = (
   currency: string,
   country: string
 ) => {
-  // Convert the amount to a number
   const amountNumber = parseFloat(amount);
 
-  // Use Intl.NumberFormat to format the price based on the locale and currency
   const formatter = new Intl.NumberFormat(country, {
     style: 'currency',
     currency: currency,
   });
 
-  // Format the price and return it
   return formatter.format(amountNumber);
 };
 
-export const formatDate = (timeString: string) =>
-  new Date(timeString).toDateString();
+export const formatDate = (timeString: string): string => {
+  const date = new Date(timeString);
+
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${day}.${month}.${year}`;
+};
